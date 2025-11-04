679dc1a43fd02ec2ac10d2821f3b223ef875efdb
NguyenTienDung <ntdug2k6@gmail.com>
2025-11-05 04:02:42 +0700

    BT

--- BT4/Bai7/src/Solution.java
+++ BT4/Bai7/src/Solution.java
@@ -1,58 +1,47 @@
-
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
+    public class ThreeSum {
+        public static List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (nums == null || nums.length < 3) return res;
 
-    static int[] findShortestReach(ArrayList<ArrayList<Integer>> graph, int start) {
-        int[] costs = new int[graph.size()];
-        int UNIT_COST = 6;
-        for (int i = 0; i < costs.length; ++i) costs[i] = -1;
+            Arrays.sort(nums);
 
-        // Viết chương trình vào đây
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-        return costs;
+                int left = i + 1;
+                int right = nums.length - 1;
+                int target = -nums[i];
+
+                while (left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            return res;
+        }
     }
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scanner = new Scanner(System.in);
-        int Q = scanner.nextInt();
-        for (int q = 0; q < Q; ++q) {
-            int N, M, start;
-            N = scanner.nextInt();
-            M = scanner.nextInt();
-            ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i <= N; ++i) {
-                graph.add(new ArrayList<Integer>());
-            }
-            for (int i = 0; i < M; ++i) {
-                int n1, n2;
-                n1 = scanner.nextInt();
-                n2 = scanner.nextInt();
-                graph.get(n1).add(n2);
-                graph.get(n2).add(n1);
-            }
-            start = scanner.nextInt();
-            /*
-            for (int i = 0; i < N; ++i) {
-                Collections.sort(graph.get(i));
-            }
-            for (int i = 1; i <= N; ++i) {
-                System.out.println("Node: " + i);
-                System.out.println(graph.get(i).toString());
-            }
-            System.out.println("Start: " + start);
-            */
-            int[] costs = findShortestReach(graph, start);
-            for (int i = 1; i < costs.length; ++i) {
-                if (i == start) continue;
-                System.out.print(costs[i] + " ");
-            }
-            System.out.println();
-        }
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> res = new ArrayList<>();
+        res = ThreeSum.threeSum(nums);
+        System.out.println(res);
     }
 }
